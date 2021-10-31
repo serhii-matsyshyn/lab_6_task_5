1165094a22de3445ec3111659516458cada0e430
Serhii Matsyshyn <serhii.matsyshyn@ucu.edu.ua>
2021-10-31 20:28:06 +0200

    function get_pure_user_words made, function get_words remade

--- target_game.py
+++ target_game.py
@@ -43,17 +43,31 @@ def get_words(filename: str,
     :rtype: List[str]
     """
     with open(filename) as file:
-        all_words = [line.rstrip() for line in file.readlines()
-                     if (len(line.rstrip()) >= 4) and (letters[4] in line)]
+        all_words = [line.rstrip() for line in file.readlines()]
 
+    return get_words_list(letters, all_words)
+
+
+def get_words_list(letters: List[str],
+                   words_from_dict: List[str]) -> List[str]:
+    """ Checks the words with rules and returns a list of words.
+
+    :param letters: Letters to check
+    :type letters: List[str]
+    :param words_from_dict: List with words from dict
+    :type words_from_dict: List[str]
+    :return: A list of words that can be found in list of letters
+    :rtype: List[str]
+    """
     output_words = []
-    for word_from_dict in all_words:
-        word_from_dict_list = list(word_from_dict)
-        for letter in letters:
-            if letter in word_from_dict_list:
-                word_from_dict_list.remove(letter)
-        if not word_from_dict_list:
-            output_words.append(word_from_dict)
+    for word_from_dict in words_from_dict:
+        if (len(word_from_dict) >= 4) and (letters[4] in word_from_dict):
+            word_from_dict_list = list(word_from_dict)
+            for letter in letters:
+                if letter in word_from_dict_list:
+                    word_from_dict_list.remove(letter)
+            if not word_from_dict_list:
+                output_words.append(word_from_dict)
 
     return output_words
 
@@ -72,13 +86,27 @@ def get_user_words() -> List[str]:
 def get_pure_user_words(user_words: List[str],
                         letters: List[str],
                         words_from_dict: List[str]) -> List[str]:
-    """
-    (list, list, list) -> list
-
-    Checks user words with the rules and returns list of those words
+    """ Checks user words with the rules and returns list of those words
     that are not in dictionary.
+
+    :param user_words: User words to check
+    :type user_words: List[str]
+    :param letters: Letters to check
+    :type letters: List[str]
+    :param words_from_dict: List with words from dict
+    :type words_from_dict: List[str]
+    :return: returns list of those words
+    that are not in dictionary
+    :rtype: List[str]
     """
-    pass
+
+    output_words = []
+    words_in_string = get_words_list(letters, words_from_dict)
+    for user_word in user_words:
+        if user_word not in words_in_string:
+            output_words.append(user_word)
+
+    return output_words
 
 
 def results():
